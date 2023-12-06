eac93e22b079b825d6ff68c9d2e3110828625ec7
Zack River <elmutamiz@gmail.com>
2023-12-06 16:15:51 +0200

    .

--- Fakarny/Fakarny/FakarnyPage.cs
+++ Fakarny/Fakarny/FakarnyPage.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using Fakarny.UserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Fakarny
@@ -8,12 +16,33 @@ namespace Fakarny
         public FakarnyPage()
         {
             InitializeComponent();
+            Add_Account add = new Add_Account();
+            addUserControl(add);
         }
 
-        private void FakarnyPage_Load(object sender, EventArgs e)
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
-    }
 
+        private void addUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            Tabs_Panel.Controls.Clear();
+            Tabs_Panel.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
+        private void Add_Acc_Button_Click(object sender, EventArgs e)
+        {
+            Add_Account add_Account = new Add_Account();
+            addUserControl(add_Account);
+        }
+
+        private void View_Contents_Button_Click(object sender, EventArgs e)
+        {
+            Edit_Account edit_Account = new Edit_Account();
+            addUserControl(edit_Account);
+        }
+    }
 }
